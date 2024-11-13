7ec2c0415d2c7b037b0762828a7dd9e863b55a3d
ysasaki1 <ysasaki1@hawk.iit.edu>
2024-11-13 13:48:09 +0900

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,19 +1,23 @@
 export const headerData = {
   links: [
     {
-      text: 'Home',
+      text: 'Homes',
       links: [
         {
-          text: 'Support Art Students',
-          href: getPermalink('/homes/support-art-students'),
+          text: 'SaaS',
+          href: getPermalink('/homes/saas'),
         },
         {
-          text: 'Artworks',
-          href: getPermalink('/homes/artworks'),
+          text: 'Startup',
+          href: getPermalink('/homes/startup'),
         },
         {
-          text: 'Become a Supporter',
-          href: getPermalink('/homes/become-a-supporter'),
+          text: 'Mobile App',
+          href: getPermalink('/homes/mobile-app'),
+        },
+        {
+          text: 'Personal',
+          href: getPermalink('/homes/personal'),
         },
       ],
     },
@@ -21,15 +25,19 @@ export const headerData = {
       text: 'Pages',
       links: [
         {
-          text: 'Features',
+          text: 'Features (Anchor Link)',
           href: getPermalink('/#features'),
+        },
+        {
+          text: 'Services',
+          href: getPermalink('/services'),
         },
         {
           text: 'Pricing',
           href: getPermalink('/pricing'),
         },
         {
-          text: 'About Us',
+          text: 'About us',
           href: getPermalink('/about'),
         },
         {
@@ -41,8 +49,37 @@ export const headerData = {
           href: getPermalink('/terms'),
         },
         {
-          text: 'Privacy Policy',
+          text: 'Privacy policy',
           href: getPermalink('/privacy'),
+        },
+      ],
+    },
+    {
+      text: 'Landing',
+      links: [
+        {
+          text: 'Lead Generation',
+          href: getPermalink('/landing/lead-generation'),
+        },
+        {
+          text: 'Long-form Sales',
+          href: getPermalink('/landing/sales'),
+        },
+        {
+          text: 'Click-Through',
+          href: getPermalink('/landing/click-through'),
+        },
+        {
+          text: 'Product Details (or Services)',
+          href: getPermalink('/landing/product'),
+        },
+        {
+          text: 'Coming Soon or Pre-Launch',
+          href: getPermalink('/landing/pre-launch'),
+        },
+        {
+          text: 'Subscription',
+          href: getPermalink('/landing/subscription'),
         },
       ],
     },
@@ -54,46 +91,75 @@ export const headerData = {
           href: getBlogPermalink(),
         },
         {
-          text: 'Art Support Stories',
-          href: getPermalink('art-support-stories', 'post'),
+          text: 'Article',
+          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
         },
         {
-          text: 'How to Support Artists',
-          href: getPermalink('how-to-support-artists', 'post'),
+          text: 'Article (with MDX)',
+          href: getPermalink('markdown-elements-demo-post', 'post'),
+        },
+        {
+          text: 'Category Page',
+          href: getPermalink('tutorials', 'category'),
+        },
+        {
+          text: 'Tag Page',
+          href: getPermalink('astro', 'tag'),
         },
       ],
     },
+    {
+      text: 'Widgets',
+      href: '#',
+    },
   ],
-  actions: [{ text: 'Join as a Supporter', href: 'https://www.artplatform.com/join', target: '_blank' }],
+  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Support',
+      title: 'Product',
       links: [
-        { text: 'How It Works', href: '#' },
-        { text: 'Benefits of Supporting', href: '#' },
-        { text: 'FAQs', href: '#' },
-        { text: 'Testimonials', href: '#' },
-        { text: 'Pricing Plans', href: '#' },
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
       ],
     },
     {
-      title: 'Community',
+      title: 'Platform',
       links: [
-        { text: 'Art Students', href: '#' },
-        { text: 'Supporters', href: '#' },
-        { text: 'Events', href: '#' },
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
       ],
     },
     {
       title: 'Company',
       links: [
-        { text: 'About Us', href: '#' },
+        { text: 'About', href: '#' },
         { text: 'Blog', href: '#' },
         { text: 'Careers', href: '#' },
-        { text: 'Contact', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
       ],
     },
   ],
@@ -102,10 +168,11 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
-    { ariaLabel: 'GitHub', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
