f9b917cd7feefc6d4160f9e39c9721043524d227
ysasaki1 <ysasaki1@hawk.iit.edu>
2024-11-14 23:53:38 +0900

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -179,6 +179,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> (仮) </a> · All rights reserved.
+    <a class="text-blue-600 underline dark:text-muted" href="https://owls-edu.com/"> (仮) </a> · All rights reserved.
   `,
 };
